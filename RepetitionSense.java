2b3ce2df95e4437c5f7b3274bc2b333f27613d70
Evelyn Zhang <40691267+milchgauss@users.noreply.github.com>
2020-11-23 12:51:37 +0100

    Add files via upload

---
+++ RepetitionSense.java
@@ -0,0 +1,35 @@
+//See if there are multiple occurrences of a letter in a word.
+// If so, replace that letter with an asterisk.
+// If not, leave it alone!
+
+
+public class RepetitionSense{
+
+static String replaceIt(String word){
+
+String encoded="";
+word=word.toLowerCase(); 
+for (int i=0;i< word.length();++i){
+char t=word.charAt(i);
+
+if(word.lastIndexOf(t)==word.indexOf(t)){
+encoded=encoded+word.charAt(i);
+}
+
+else{
+encoded=encoded+"*";
+}
+}
+System.out.println(encoded);
+return encoded;
+}
+
+public static void main(String[]args){
+
+RepetitionSense p= new RepetitionSense();
+p.replaceIt("Lalaaat");
+p.replaceIt("rat");
+p.replaceIt("reeeaaaaaapeat");
+
+}
+}
